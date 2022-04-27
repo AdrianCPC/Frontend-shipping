a71fea942d0fda2cffae07bb64244bd0c4230b32
AdrianPC <adrianchooo18@gmail.com>
2022-04-27 11:38:17 -0500

    routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShippingCreateComponent } from './components/shipping-create/shipping-create.component';
+import { ShippingEditComponent } from './components/shipping-edit/shipping-edit.component';
+import { ShippingDeleteComponent } from './components/shipping-delete/shipping-delete.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'create-shipping' },
+  { path: 'create-shipping', component: ShippingCreateComponent },
+  { path: 'edit-shipping/:id', component: ShippingEditComponent },
+  { path: 'shipping-delete', component: ShippingDeleteComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
